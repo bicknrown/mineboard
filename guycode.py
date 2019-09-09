56899c73d5cfab9d6d762a22e735b347a0499a66
Nick Brown <bicknrown@gmail.com>
2019-09-08 21:10:39 -0400

    working piano keyboard for text input, windows hook stops input from reaching game.

--- guycode.py
+++ guycode.py
@@ -5,11 +5,18 @@
 #################################################################
 
 import pygame.midi
+import pynput
+from pynput.keyboard import Key, Controller
 
+keyboard = Controller()
 piano_list = []  # creates list for midi input (49)
 for i in range(49):
     i = 36 + i
-    piano_list.append(i)
+    piano_list.append(i)  # piano input list
+number_list = []
+for i in range(10):
+    i = i
+    number_list.append(str(i))
 
 
 def letter_range(start, stop="{", step=1):  # creates list of keyboard letters
@@ -17,16 +24,21 @@ def letter_range(start, stop="{", step=1):  # creates list of keyboard letters
         yield chr(ord_)
 
 
-type_list = list(letter_range("a", "z"))  # also creates list of keyboard letters
-type_list.append('z')
-print(type_list)
+z_list = ['z']
+
+add_list = [',', '.', '/', ';', "'", '[', ']', '-', Key.space, Key.shift_l, Key.esc, Key.ctrl, Key.enter]
+
+type_list = list(letter_range("a", "z")) + z_list + number_list + add_list  # also creates list of keyboard letters
 
 
 def readInput(input_device):  # read midi device input
     while True:
         if input_device.poll():
             event = input_device.read(1)
-            print(event)
+            if event[0][0][2] != 0:
+                keyboard.press(type_list[piano_list.index(event[0][0][1])])
+            if event[0][0][2] == 0:
+                keyboard.release(type_list[piano_list.index(event[0][0][1])])
 
 
 if __name__ == '__main__':  # also reads midi device input
